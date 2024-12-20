c12a23025540a04838bf475dba4cbde2d9e6ba38
Acapella24365 <syedahayaamzahra@gmail.com>
2024-12-20 15:16:11 +0500

    Hey!

---
+++ ..py
@@ -0,0 +1,11 @@
+n1 = 45
+n2 = 2
+
+print("Sum", n1 + n2)
+print("Subtract", n1 - n2)
+print("Multiply", n1 * n2)
+print("Divide", n1 / n2)
+print("Floor Division", n1 // n2)
+print("Modulus", n1 % n2)
+print("Square", n1 ** 2)
+print("Square Root", n1 ** 0.5)
---
+++ .py
@@ -0,0 +1,20 @@
+name = "Hayaam"
+age = 11
+Gender_Girl = True
+Weight = 35.8
+
+print("Name:", name)
+print("Data Type of name is:", type(name))
+print("Age:", age)
+print("Data Type of age is:", type(age))
+print("Weight:", Weight)
+print("Data Type of weight is:", type(Weight))
+print("Gender is Girl:", (Gender_Girl))
+
+print("\n after Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is:", type(age))
+Weight = int(Weight)
+print(Weight)
+print("Data Type of age is:", type(Weight))
